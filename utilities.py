a6003b3cfd1b20229e4a5ca226bd85e593eba1e0
Mechap <simongregoire007@gmail.com>
2020-05-27 12:27:20 +0200

    color

---
+++ utilities.py
@@ -0,0 +1,17 @@
+class Color:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+
+    SKY_BLUE = (41, 173, 255)
+    GRASS_GREEN = (0, 168, 68)
+    OCEAN_BLUE = (60, 188, 252)
+    DARK_BLUE = (0, 64, 88)
+    WOOD_BROWN = (172, 124, 0)
+    GRAY = (120, 120, 120)
+    LIGHT_GRAY = (188, 188, 188)
+    LIGHT_BLUE = (104, 136, 252)
+    LIGHT_PINK = (248, 164, 192)
+    TEAL = (0, 136, 136)
